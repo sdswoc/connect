bdbe3fd63163a61ad18afa80c3102976e83d0698
aryaman-titan <aryamanbehera@gmail.com>
2019-12-12 02:35:28 +0530

    Successfully updated Connect Feature

--- NewConnect.php
+++ NewConnect.php
@@ -20,9 +20,14 @@ include('server.php');
 
      if(mysqli_num_rows($redundancy_check) == 0){
         $sql = "INSERT INTO followerData (userID, followerID) VALUES($id_to_be_followed,$follower_id)";
-   mysqli_query($db, $sql);
-     }
-    
+       if(mysqli_query($db, $sql)){
+         $following_status = "Following";
+         echo "<script>
+              document.getElementById('follow_status').innerHTML = ".$following_status."
+               </script>";
+
+       }  
+  }    
    }
     
   
@@ -79,26 +84,44 @@ include('server.php');
 <th>Username</th>
 
 <th>Bio</th>
-
+<th>Follow Status</th>
 </tr>
 <?php
 $conn = mysqli_connect("localhost", "root", "abiit@2019", "rconnect");
 // Check connection
 
+function follow_status($conn, $id, $self_ID){
+
+  $query = "SELECT * from followerData where userID = $id AND followerID = $self_ID";
+  $check = mysqli_query($conn, $query);
+  if(mysqli_num_rows($check)==1){
+    $msg_return = "Following";
+    $friend_query = "SELECT * from followerData where userID = $self_ID AND followerID = $id";
+    $check_2 = mysqli_query($conn, $friend_query);
+    if(mysqli_num_rows($check_2)==1){
+      $msg_return = "Friends";
+    }
+  }
+  else{$msg_return = "  Follow  ";}
+  return $msg_return;
+}
+  
 $sql = "SELECT userID, name, username, bio FROM userData";
 $result = mysqli_query($conn, $sql);
 if ($result->num_rows > 0) {
 // output data of each row
 while($row = $result->fetch_assoc()) {
 $id = $row["userID"];
+$self_ID = $_SESSION['id'];
 echo "<tr><td>" . $row["userID"]. "</td><td>" . $row["name"] . "</td><td>" . $row["username"] . "</td><td>" . $row["bio"] . "</td>";
 echo "<td>";
-echo "<a class = 'google' href = '  NewConnect.php?id=".$id."'>&nbsp;Connect&nbsp;&nbsp;</a>";
+$msg = follow_status($conn, $id, $self_ID);
+echo "<a class = 'google follow_status' style = 'padding-left: 5px; padding-right: 5px;' href = 'NewConnect.php?id=".$id."'>".$msg."</a>";
 echo "</td>";
 }
 } 
 else { echo "0 results"; }
-$conn->close();
+$conn->close(); 
 ?>
 </tr>
 
